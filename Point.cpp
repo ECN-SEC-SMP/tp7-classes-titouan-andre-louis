eb375d04f962d012cc42875e6b20a3013ad36a9f
AndreLouisSt <stephantandrelouis@gmail.com>
2025-01-31 08:38:01 +0000

    Test point.cpp

--- Point.cpp
+++ Point.cpp
@@ -5,16 +5,3 @@ void PointPlan::translater(int X, int Y) {
         x=x+X;
         y=y+Y;
     }
-
-    InitPoint();
-    Parametre(int a, int b);
-    Recopier(Point const &point);
-
-    // Accesseurs
-    int getX();
-    int getY();
-
-    // Mutateurs
-    void setX(x);
-    void setY(y);
-
