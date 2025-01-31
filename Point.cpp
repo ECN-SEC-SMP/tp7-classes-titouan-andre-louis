c3e5663817a51501593958cc0eeca5344a6cbe1a
AndreLouisSt <stephantandrelouis@gmail.com>
2025-01-31 09:19:38 +0000

    Co-authored-by: Titwo <Ti-two@users.noreply.github.com>

--- Point.cpp
+++ Point.cpp
@@ -2,18 +2,30 @@
 #include <iostream>
 #include "Point.h"
 
-void PointPlan::translater(int X, int Y) {
-    x=x+X;
-    y=y+Y;
+void PointPlan::translater(int X, int Y)
+{
+    this->x = x + X;
+    this->y = y + Y;
+}
+
+void PointPlan::translater(PointPlan p) {
+    this->x = 0;
+    this->y = 0;
 }
 
 PointPlan::PointPlan(int a, int b)
 {
-    this->x = x;
-    this->y = y;
+    this->x = a;
+    this->y = b;
 }
 
 PointPlan::PointPlan()
+{
+    this->x = 0;
+    this->y = 0;
+}
+
+PointPlan::PointPlan(PointPlan const &point)
 {
     this->x = 0;
     this->y = 0;
--- main.cpp
+++ main.cpp
@@ -6,15 +6,19 @@
 using namespace std;
 
 // main
-int main() {
-    // PointPlan point1(10,5);
-    PointPlan point1();
+int main()
+{
+    // PointPlan point1(point1);    // OK
+    PointPlan point1(10,5);      // OK
+    // PointPlan point1();          // OK?
 
     point1.setX(10);
     point1.setY(5);
 
+    point1.translater(3,2);
+
     cout << point1.getX() << endl;
     cout << point1.getY() << endl;
-    
+
     return 0;
 }
